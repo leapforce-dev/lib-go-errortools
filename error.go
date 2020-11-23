0455dad394fb218768acf262900ab0a68438e598
caselongo <caselongo@gmail.com>
2020-11-23 21:52:46 +0100

    x

--- error.go
+++ error.go
@@ -15,13 +15,32 @@ type Error struct {
 // ErrorMessage return message-only Error
 //
 func ErrorMessage(stringOrError interface{}) *Error {
+	return &Error{Message: message(stringOrError)}
+}
+
+func message(stringOrError interface{}) string {
+	if stringOrError == nil {
+		return ""
+	}
 	errError, ok := stringOrError.(error)
 	if ok {
-		return &Error{Message: errError.Error()}
+		return errError.Error()
 	}
 	errString, ok := stringOrError.(string)
 	if ok {
-		return &Error{Message: errString}
+		return errString
 	}
-	return &Error{Message: "Invalid type passed to ErrorMessage."}
+	return "Invalid type passed to ErrorMessage."
+}
+
+func (err *Error) SetRequest(request *http.Request) {
+	(*err).Request = request
+}
+
+func (err *Error) SetResponse(response *http.Response) {
+	(*err).Response = response
+}
+
+func (err *Error) SetMessage(stringOrError interface{}) {
+	(*err).Message = message(stringOrError)
 }
